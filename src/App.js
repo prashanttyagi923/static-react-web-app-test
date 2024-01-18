731ff7faa47bc06d0578c4b5195b35a41be30a26
Prashant Tyagi <prashant.tyagi@crowe.com>
2024-01-18 17:49:26 +0530

    updated some code

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { ApplicationInsights } from '@microsoft/applicationinsights-web'
 
 function App() {
   
 const appInsights = new ApplicationInsights({ config: {
-  connectionString: 'InstrumentationKey=655daf8d-e697-4d5a-82cf-9e5528d6292c;IngestionEndpoint=https://eastus-8.in.applicationinsights.azure.com/;LiveEndpoint=https://eastus.livediagnostics.monitor.azure.com/'
+  connectionString: 'InstrumentationKey=655daf8d-e697-4d5a-82cf-9e5528d6292c;IngestionEndpoint=https://eastus-8.in.applicationinsights.azure.com/;LiveEndpoint=https://eastus.livediagnostics.monitor.azure.com/',
+  enableDebug: true,
+  enableAutoRouteTracking: true
 } });
-appInsights.loadAppInsights();
-appInsights.trackPageView();
+useEffect(()=>{
+  appInsights.loadAppInsights();
+  appInsights.trackPageView();
+},[]);
   const value = 'World';
   return <div>Hello {value}</div>;
 }
