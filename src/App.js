c3d1d62e5515483887ac261a82fe2699c37b778b
Prashant Tyagi <Prashant.Tyagi@crowe.com>
2024-01-18 17:29:26 +0530

    added app insights

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import React from 'react';
+import { ApplicationInsights } from '@microsoft/applicationinsights-web'
 
 function App() {
+  
+const appInsights = new ApplicationInsights({ config: {
+  connectionString: 'InstrumentationKey=655daf8d-e697-4d5a-82cf-9e5528d6292c;IngestionEndpoint=https://eastus-8.in.applicationinsights.azure.com/;LiveEndpoint=https://eastus.livediagnostics.monitor.azure.com/'
+} });
+appInsights.loadAppInsights();
+appInsights.trackPageView();
   const value = 'World';
   return <div>Hello {value}</div>;
 }
